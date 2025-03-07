b3c24157ed2481c3271583507a3860d1927fa70b
infodoug <douglasrafael.info@gmail.com>
2025-03-07 11:06:22 -0300

    posiciona a descrição do projeto

--- src/components/Projeto.jsx
+++ src/components/Projeto.jsx
@@ -1,20 +1,20 @@
 import styles from "./Projeto.module.css";
 import cintTitle from "../img/cint-title.png";
 import cintScreen1 from "../img/cint-screen-1.png";
+import cintScreen2 from "../img/cint-screen-2.png";
+import cintScreen3 from "../img/cint-screen-3.png";
 
-function Projeto({backgroundComponent, descriptionComponent}) {
+function Projeto({backgroundComponent}) {
     return (
         <div className={styles.backgroundComponent}>
             {backgroundComponent}
-            <div className={styles.descriptionComponent}>
-                {descriptionComponent}
-            </div>
+
         </div>
     )
 }
 
 
-function CintBackground() {
+function CintBackground({descriptionContainer}) {
     return (
         <div className={styles.cintBackground}>
             <div className={styles.cintTitle}>
@@ -23,6 +23,12 @@ function CintBackground() {
 
             <div className={styles.cintScreens}>
                 <img src={cintScreen1} alt="Falha ao carregar imagem" />
+                <img src={cintScreen2} alt="Falha ao carregar imagem" />
+                <img src={cintScreen3} alt="Falha ao carregar imagem" />
+            </div>
+
+            <div className={styles.descriptionContainer}>
+                <div className={styles.descriptionComponent}></div>
             </div>
         </div>
     )
